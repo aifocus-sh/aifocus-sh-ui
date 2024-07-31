4fc4e228c199b3e7c528f232ed1f20ab7540ae7c
Ivan Montes <ivan.montes.developer@gmail.com>
2024-07-31 17:16:56 -0300

    changes

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,19 @@ export const metadata: Metadata = {
   title: "Unlocking Creativity with AIfocus.sh - Artificial Intelligence in Writing",
   description:
     "Learn how AIfocus.sh's cutting-edge AI technology empowers writers to produce high-quality content quickly and efficiently, revolutionizing the way you create digital media articles.",
+  "openGraph": {
+    title: "Unlocking Creativity with AIfocus.sh - Artificial Intelligence in Writing",
+    description: "Learn how AIfocus.sh's cutting-edge AI technology empowers writers to produce high-quality content quickly and efficiently, revolutionizing the way you create digital media articles.",
+    url: "https://aifocus.sh/",
+    siteName: "AIfocus.sh",
+    images: [
+      {
+        url: "https://aifocus.sh/images/introduction-to-aifocus-sh.jpg",
+        width: 1280,
+        height: 800,
+      },
+    ],
+  }
 };
 
 export default function Page() {
